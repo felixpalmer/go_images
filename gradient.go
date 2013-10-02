5f966081fd9da13a64ac2adcd16f8f50edaa8aac
Felix Palmer <felixpalmer@gmail.com>
2013-10-02 13:02:20 +0200

    Gradient drawing

---
+++ gradient.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/png"
+  "log"
+	"os"
+)
+
+func main() {
+	width, height := 128, 128
+	m := image.NewRGBA(image.Rect(0, 0, width, height))
+  draw_gradient(*m)
+  out_filename := "gradient.png"
+	out_file, err := os.Create(out_filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer out_file.Close()
+  log.Print("Saving image to: ", out_filename)
+	png.Encode(out_file, m)
+}
+
+func draw_gradient(m image.RGBA) {
+	size := m.Bounds().Size()
+	for x := 0; x < size.X; x++ {
+		for y := 0; y < size.Y; y++ {
+			color := color.RGBA{
+				uint8(255 * x / size.X),
+				uint8(255 * y / size.Y),
+				55,
+				255}
+			m.Set(x, y, color)
+		}
+	}
+}
