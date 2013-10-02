e7041bf0468c7a94312d2d20a7601ea1db7a26d3
Felix Palmer <felixpalmer@gmail.com>
2013-10-02 12:23:20 +0200

    Initial commit

---
+++ blank.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"image"
+	"image/png"
+  "log"
+	"os"
+)
+
+func main() {
+	width, height := 128, 128
+	m := image.NewRGBA(image.Rect(0, 0, width, height))
+  out_filename := "blank.png"
+	out_file, err := os.Create(out_filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer out_file.Close()
+  log.Print("Saving image to: ", out_filename)
+	png.Encode(out_file, m)
+}
