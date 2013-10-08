fd7a4cee5e051fccb861e610ebbf9da129d7981a
Felix Palmer <felixpalmer@gmail.com>
2013-10-08 11:28:57 +0200

    Add vector type

---
+++ vector.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+  "image"
+  "math"
+)
+
+type Vector struct {
+	X, Y float64
+}
+
+func (v *Vector) Rotate(angle float64) {
+	cos, sin := math.Cos(angle), math.Sin(angle)
+	v.X, v.Y = v.X*cos+v.Y*sin, v.Y*cos-v.X*sin
+}
+
+func (v *Vector) Scale(k float64) {
+	v.X, v.Y = v.X*k, v.Y*k
+}
+
+func (v *Vector) Add(v2 Vector) Vector {
+	return Vector{v.X + v2.X, v.Y + v2.Y}
+}
+
+func (v *Vector) toPoint() image.Point {
+	return image.Point{int(v.X), int(v.Y)}
+}
