1a0bed1753e7d0845031ffa5e54ecea32edf1779
Felix Palmer <felixpalmer@gmail.com>
2013-10-16 12:50:59 +0200

    Refactor lines.go and spirals.go to not include canvas

--- canvas.go
+++ canvas.go
@@ -28,3 +28,27 @@ func (c Canvas) DrawGradient() {
 		}
 	}
 }
+
+func (c Canvas) DrawLine(color color.RGBA, from Vector, to Vector) {
+	delta := to.Sub(from)
+	length := delta.Length()
+	x_step, y_step := delta.X/length, delta.Y/length
+	limit := int(length + 0.5)
+	for i := 0; i < limit; i++ {
+		x := from.X + float64(i)*x_step
+		y := from.Y + float64(i)*y_step
+		c.Set(int(x), int(y), color)
+	}
+}
+
+func (c Canvas) DrawSpiral(color color.RGBA, from Vector) {
+	dir := Vector{0, 5}
+	last := from
+	for i := 0; i < 10000; i++ {
+		next := last.Add(dir)
+		c.DrawLine(color, last, next)
+		dir.Rotate(0.03)
+		dir.Scale(0.999)
+		last = next
+	}
+}
