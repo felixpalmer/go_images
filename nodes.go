ebdf52ef397265b11954c31c27b8b32cd0e94d45
Felix Palmer <felixpalmer@gmail.com>
2013-10-30 15:16:41 +0100

    Some sort of propagation working, seems bugged though

--- nodes.go
+++ nodes.go
@@ -7,6 +7,7 @@ import (
 	"log"
   "math/rand"
 	"os"
+  "sort"
   "time"
 )
 
@@ -17,29 +18,41 @@ func main() {
 
   // Create and populate the slice of Nodes
   n := 1000
-  peers := 8
+  peers := 4
   nodes := make([]*Node, n)
   for i := 0; i < n; i++ {
     nodes[i] = NewNode(peers, canvas)
   }
 
   // Randomly point Nodes at each other
+  // for _, node := range nodes {
+  //   for _, j := range rand.Perm(n)[:peers] {
+  //     node.Peers = append(node.Peers, nodes[j])
+  //   }
+  // }
+
+  nodesCopy := make([]*Node, n)
+  copy(nodesCopy, nodes)
   for _, node := range nodes {
-    for _, j := range rand.Perm(n)[:peers] {
-      node.Peers = append(node.Peers, nodes[j])
-    }
+    // Sort the nodes by distance
+    sorter := NodeSorter{nodesCopy, node}
+    sort.Sort(sorter)
+    node.Peers = append(node.Peers, nodesCopy[1:peers+1]...)
   }
 
   // Draw connections between nodes
-  // for _, node := range nodes {
-  //   for _, peer := range node.Peers {
-  //     canvas.DrawLine(color.RGBA{0, 0, 0, 255}, node.Position, peer.Position)
-  //   }
-	// }
+  for _, node := range nodes {
+    for _, peer := range node.Peers {
+      canvas.DrawLine(color.RGBA{0, 0, 0, 60}, node.Position, peer.Position)
+    }
+	}
 
   // Start sending messages between nodes
-  nodes[0].Ch <- nodes[1]
-  //time.Sleep(time.Second / 10)
+  for i := 0; i < 10; i++ {
+    nodes[i].Power = 255
+    nodes[i].Ch <- nodes[i].Peers[0]
+  }
+  time.Sleep(time.Second)
 
   // Write out image
 	outFilename := "nodes.png"
@@ -57,6 +70,7 @@ type Node struct {
 	Ch chan *Node
   Peers []*Node
   Canvas *Canvas
+  Power uint8
 }
 
 func NewNode(peers int, canvas *Canvas) *Node {
@@ -68,6 +82,7 @@ func NewNode(peers int, canvas *Canvas) *Node {
   node.Position = Vector{x, y}
   node.Canvas = canvas
   node.Ch = make(chan *Node)
+  node.Power = 0
   go node.Listen()
   return node
 }
@@ -76,12 +91,36 @@ func (n *Node) Listen() {
   // Listen for incoming connection on node's channel
   for {
     peer := <-n.Ch
-
-    n.Canvas.DrawLine(color.RGBA{255, 0, 0, 255}, n.Position, peer.Position)
+    peer.Power -= 5
+    n.Power = peer.Power
+    n.Canvas.DrawLine(color.RGBA{255, n.Power, 0, 255}, n.Position, peer.Position)
     // Retransmit to random node
-    // n.Peers[rand.Intn(len(n.Peers))].Ch <- n
-    for _, p := range n.Peers {
-      p.Ch <- n
+    if n.Power > 0 {
+      go n.Send()
     }
   }
+}
+
+func (n *Node) Send() {
+  for _, target := range n.Peers {
+    if target.Power == 0 {
+      target.Ch <- n
+      break
+    }
+  }
+}
+
+type NodeSorter struct {
+  data []*Node
+  target *Node
+}
+
+func (sorter NodeSorter) Len() int { return len(sorter.data) }
+func (sorter NodeSorter) Less(i, j int) bool {
+  iDelta := sorter.data[i].Position.Sub(sorter.target.Position)
+  jDelta := sorter.data[j].Position.Sub(sorter.target.Position)
+  return iDelta.Length() < jDelta.Length()
+}
+func (sorter NodeSorter) Swap(i, j int) {
+  sorter.data[i], sorter.data[j] = sorter.data[j], sorter.data[i]
 }
