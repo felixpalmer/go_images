8c685b260e3e6180fc5681cdea8e52a696fcbd2f
Felix Palmer <felixpalmer@gmail.com>
2013-11-08 16:44:15 +0100

    Tweaks to simple server

--- simple_server.go
+++ simple_server.go
@@ -1,36 +1,37 @@
 package main
 
 import (
-  "io"
-  "net/http"
+	"io"
+	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
 func HelloHandler(writer http.ResponseWriter, request *http.Request) {
 	vars := mux.Vars(request)
-  if vars["name"] != "" {
-    io.WriteString(writer, "hello " + vars["name"] + "!")
-  } else {
-    io.WriteString(writer, "hello!")
-  }
+	if vars["name"] != "" {
+		io.WriteString(writer, "hello "+vars["name"]+"!")
+	} else {
+		io.WriteString(writer, "hello!")
+	}
 }
 
 func NoCacheDecorator(h http.Handler) http.Handler {
-  return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
 		w.Header().Set("Pragma", "no-cache")
 		w.Header().Set("Expires", "0")
-    h.ServeHTTP(w, r)
-  })
+		h.ServeHTTP(w, r)
+	})
 }
 
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/hello", HelloHandler)
 	router.HandleFunc("/hello/{name}", HelloHandler)
-  staticHandler := http.StripPrefix("/", http.FileServer(http.Dir(".")))
-  staticHandler = NoCacheDecorator(staticHandler)
-	router.PathPrefix("/").Handler(staticHandler)
-  http.ListenAndServe("localhost:1234", router)
+	staticHandler := http.FileServer(http.Dir("."))
+	staticHandler = http.StripPrefix("/static/", staticHandler)
+	staticHandler = NoCacheDecorator(staticHandler)
+	router.PathPrefix("/static/").Handler(staticHandler)
+	http.ListenAndServe("localhost:1234", router)
 }
