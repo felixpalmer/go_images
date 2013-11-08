159419b8f2f52c627518a0f081044c1da9d842cd
Felix Palmer <felixpalmer@gmail.com>
2013-11-08 14:40:28 +0100

    Add no cache option

--- simple_server.go
+++ simple_server.go
@@ -11,11 +11,20 @@ func HelloHandler(writer http.ResponseWriter, request *http.Request) {
   io.WriteString(writer, "hello!")
 }
 
+func NoCacheDecorator(h http.Handler) http.Handler {
+  return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+		w.Header().Set("Pragma", "no-cache")
+		w.Header().Set("Expires", "0")
+    h.ServeHTTP(w, r)
+  })
+}
+
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/hello", HelloHandler)
   staticHandler := http.StripPrefix("/", http.FileServer(http.Dir(".")))
-  //staticHandler = NoCache(staticHandler)
+  staticHandler = NoCacheDecorator(staticHandler)
 	router.PathPrefix("/").Handler(staticHandler)
   http.ListenAndServe("localhost:1234", router)
 }
