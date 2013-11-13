c799012db76b0c1bdd3eb429507d8186ec47d931
Felix Palmer <felixpalmer@gmail.com>
2013-11-13 12:47:42 +0100

    Simple sine wave animation

--- ws_canvas.go
+++ ws_canvas.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
   "image"
   "io"
+  "math"
 	"net/http"
   "time"
 
@@ -15,17 +16,19 @@ func WebsocketHandler(ws *websocket.Conn) {
 	width, height := 200, 200
 	canvas := NewCanvas(image.Rect(0, 0, width, height))
 	canvas.DrawGradient()
+  t := 0.0
   for {
     msg := ""
     for x := 0; x < width; x++ {
-      for y := 0; y < width; y++ {
-        r, g, b, _ := canvas.At(x, y).RGBA()
-        color := fmt.Sprintf("#%02x%02x%02x", r/0xFF, g/0xFF, b/0xFF)
-        msg += fmt.Sprintf("PIXEL %d %d %s\n", x, y, color)
-      }
+      y := height/2 + int(50 * math.Sin(t/10) * math.Sin(float64(x)/10.0 + t))
+      //r, g, b, _ := canvas.At(x, y).RGBA()
+      //color := fmt.Sprintf("#%02x%02x%02x", r/0xFF, g/0xFF, b/0xFF)
+      color := "#ff0000"
+      msg += fmt.Sprintf("PIXEL %d %d %s\n", x, y, color)
     }
+    t += 0.3
     io.WriteString(ws, msg)
-    time.Sleep(10 * time.Second)
+    time.Sleep(time.Second / 30.0)
   }
 }
 
