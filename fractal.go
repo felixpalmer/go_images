504a3fcbfa5fa9e82db7e4be54dfdc9db8824a82
Felix Palmer <felixpalmer@gmail.com>
2013-11-20 11:46:50 +0100

    Move fractal drawing into separate function and generate examples

--- fractal.go
+++ fractal.go
@@ -5,7 +5,7 @@ import (
 	"image/color"
 	"image/png"
 	"log"
-  "math"
+	"math"
 	"math/cmplx"
 	"os"
 )
@@ -16,7 +16,7 @@ import (
 // A zoom of 1 means one pixel correspond to one unit in
 // the complex plane
 func toCmplx(x, y int, zoom float64, center complex128) complex128 {
-	return center + complex(float64(x) / zoom, float64(y) / zoom)
+	return center + complex(float64(x)/zoom, float64(y)/zoom)
 }
 
 // Perform iter iterations using the mandelbrot algorithm, and return
@@ -25,9 +25,9 @@ func mandelbrot(c complex128, iter int) float64 {
 	z := complex(0, 0)
 	for i := 0; i < iter; i++ {
 		z = z*z + c
-    if cmplx.Abs(z) > 1000 {
-      return 1000
-    }
+		if cmplx.Abs(z) > 1000 {
+			return 1000
+		}
 	}
 	return cmplx.Abs(z)
 }
@@ -35,31 +35,34 @@ func mandelbrot(c complex128, iter int) float64 {
 // Creates a function for converting a magnitude into a color
 // based on a gradient image file
 func createColorizer(filename string) func(float64) color.Color {
-  gradient := CanvasFromFile(filename)
-  limit := gradient.Bounds().Size().Y - 1
-  return func(mag float64) color.Color {
-    // Clamp magnitude to size of gradient
-    m := int(math.Max(math.Min(300*mag, float64(limit)), 1))
-    return gradient.At(0, m)
-  }
+	gradient := CanvasFromFile(filename)
+	limit := gradient.Bounds().Size().Y - 1
+	return func(mag float64) color.Color {
+		// Clamp magnitude to size of gradient
+		m := int(math.Max(math.Min(300*mag, float64(limit)), 1))
+		return gradient.At(0, m)
+	}
+}
+
+func drawFractal(canvas *Canvas, zoom float64, center complex128, colorizer func(float64) color.Color) {
+	size := canvas.Bounds().Size()
+	for x := 0; x < size.X; x++ {
+		for y := 0; y < size.Y; y++ {
+			c := toCmplx(x-size.X/2, y-size.Y/2, zoom, center)
+			mag := mandelbrot(c, 50)
+			color := colorizer(mag)
+			canvas.Set(x, y, color)
+		}
+	}
 }
 
 func main() {
 	width, height := 800, 600
 	canvas := NewCanvas(image.Rect(0, 0, width, height))
-  colorizer := createColorizer("fractalGradients/gradient3.png")
-
-  zoom := 22400.0
-  center := complex(-0.55, 0.6)
-	for x := 0; x < width; x++ {
-		for y := 0; y < width; y++ {
-      c := toCmplx(x - width / 2, y - height / 2, zoom, center)
-      mag := mandelbrot(c, 50)
-			color := colorizer(mag)
-			canvas.Set(x, y, color)
-		}
-	}
-
+	zoom := 16000.0
+	center := complex(-0.71, -0.25)
+	colorizer := createColorizer("fractalGradients/gradient1.png")
+	drawFractal(canvas, zoom, center, colorizer)
 
 	outFilename := "fractal.png"
 	outFile, err := os.Create(outFilename)
