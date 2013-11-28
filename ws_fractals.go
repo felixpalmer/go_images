cc81e47bf777b4320e8c43545ef4226022c8921a
Felix Palmer <felixpalmer@gmail.com>
2013-11-28 11:54:54 +0100

    Convert to base64 and draw fractal in browser

--- ws_fractals.go
+++ ws_fractals.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+  "bytes"
+  "encoding/base64"
 	"fmt"
+  "image"
+  "image/png"
 	"net/http"
+  "io"
 
 	"code.google.com/p/go.net/websocket"
 	"github.com/gorilla/mux"
 )
 
 func UIHandler(ws *websocket.Conn) {
+	width, height := 800, 600
+	canvas := NewCanvas(image.Rect(0, 0, width, height))
+	zoom := 16000.0
+	center := complex(-0.71, -0.25)
+	colorizer := createColorizer("fractalGradients/gradient1.png")
+
+
 	for {
     var msg string
     err := websocket.Message.Receive(ws, &msg)
@@ -17,6 +29,15 @@ func UIHandler(ws *websocket.Conn) {
       break
     }
     fmt.Println(msg)
+
+    // Create fractal and convert to base64
+    drawInvMandelbrot(canvas, zoom, center, colorizer)
+    imgBuf := new(bytes.Buffer)
+    imgEncoder := base64.NewEncoder(base64.StdEncoding, imgBuf)
+    png.Encode(imgEncoder, canvas)
+    imgEncoder.Close()
+    drawMsg := fmt.Sprintf("DRAW %s\n", imgBuf)
+    io.WriteString(ws, drawMsg)
   }
 }
 
