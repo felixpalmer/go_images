bfe97d85c9e9a3269c0760a0556771a23e645d5f
Felix Palmer <felixpalmer@gmail.com>
2013-12-01 17:21:00 +0100

    Format ws_fractal code

--- ws_fractals.go
+++ ws_fractals.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-  "image"
+	"image"
+	"io"
 	"net/http"
-  "io"
-  "strconv"
-  "strings"
+	"strconv"
+	"strings"
 
 	"code.google.com/p/go.net/websocket"
 	"github.com/gorilla/mux"
@@ -20,32 +20,32 @@ func UIHandler(ws *websocket.Conn) {
 	colorizer := createColorizer("fractalGradients/gradient2.png")
 
 	for {
-    var msg string
-    websocket.Message.Receive(ws, &msg)
+		var msg string
+		websocket.Message.Receive(ws, &msg)
 
-    // Parse message
-    fields := strings.Fields(msg)
-    cmd := fields[0]
-    if cmd == "ZOOMIN" {
-      zoom *= 2.0
-    } else if cmd == "ZOOMOUT" {
-      zoom /= 2.0
-    } else if cmd == "MOUSEDOWN" {
-      x, _ := strconv.ParseInt(fields[1], 10, 0)
-      y, _ := strconv.ParseInt(fields[2], 10, 0)
-      center = toCmplx(int(x)-width/2, int(y)-height/2, zoom, center)
-    } else if cmd == "SETGRADIENT" {
-      colorizer = createColorizer(fields[1])
-    } else {
-      fmt.Println("Unknown command:", cmd)
-      fmt.Println("Message:", msg)
-    }
+		// Parse message
+		fields := strings.Fields(msg)
+		cmd := fields[0]
+		if cmd == "ZOOMIN" {
+			zoom *= 2.0
+		} else if cmd == "ZOOMOUT" {
+			zoom /= 2.0
+		} else if cmd == "MOUSEDOWN" {
+			x, _ := strconv.ParseInt(fields[1], 10, 0)
+			y, _ := strconv.ParseInt(fields[2], 10, 0)
+			center = toCmplx(int(x)-width/2, int(y)-height/2, zoom, center)
+		} else if cmd == "SETGRADIENT" {
+			colorizer = createColorizer(fields[1])
+		} else {
+			fmt.Println("Unknown command:", cmd)
+			fmt.Println("Message:", msg)
+		}
 
-    // Create fractal and convert to base64
-    drawInvMandelbrot(canvas, zoom, center, colorizer)
-    drawMsg := fmt.Sprintf("DRAW %s", canvas.ToBase64())
-    io.WriteString(ws, drawMsg)
-  }
+		// Create fractal and convert to base64
+		drawInvMandelbrot(canvas, zoom, center, colorizer)
+		drawMsg := fmt.Sprintf("DRAW %s", canvas.ToBase64())
+		io.WriteString(ws, drawMsg)
+	}
 }
 
 func main() {
